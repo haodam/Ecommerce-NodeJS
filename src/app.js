4d7bee0b89274807b57130aa038d7413501c907d
dam hao <damanhhao3004@gmail.com>
2023-08-10 17:22:08 +0700

    init commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan')
+const { default: helmet} = require('helmet')
 const app = express()
 
 // init middleware
+app.ues(morgan("dev"))
+app.use(helmet())
+
 
 // init db
 
